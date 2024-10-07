dbe0948f5a000e2fe140f482c7da803bf5daa462
cybardev <sheikh@cybar.dev>
2024-10-07 00:49:24 -0300

    re: use high-contrast theme

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -3,7 +3,8 @@
 -- USER CONFIGURATION --
 -- ~/.config/nvim/lua/plugins/user.lua
 
-local BG_COLOR = "#202020"
+local BG_DARK = "#202020"
+local BG_DARKER = "#111111"
 
 ---@type LazySpec
 return {
@@ -35,16 +36,20 @@ return {
     "AstroNvim/astroui",
     ---@type AstroUIOpts
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "onedark_dark",
       highlights = {
         init = { -- this table overrides highlights in all themes
           -- Normal = { bg = "#000000" },
         },
         onedark = {
-          CursorLine = { bg = BG_COLOR },
+          CursorLine = { bg = BG_DARK },
           CursorLineNr = { bg = "#2D2D2D", fg = "#EEEEEE" },
-          LineNr = { bg = BG_COLOR },
-          ColorColumn = { bg = BG_COLOR },
+          LineNr = { bg = BG_DARK },
+          ColorColumn = { bg = BG_DARK },
+        },
+        onedark_dark = {
+          CursorLine = { bg = BG_DARKER },
+          LineNr = { bg = BG_DARKER },
         },
       },
     },
